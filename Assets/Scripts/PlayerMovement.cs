eaf87116520a8f13c4bdb63e694d3b4eace46a06
RyanDeBarros <ryan2004db@gmail.com>
2025-09-20 13:43:41 -0400

    Split player script into movement and camera components

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,50 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-// TODO separate into PlayerMovement and PlayerCamera scripts.
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Camera cam;
-    [SerializeField] private CharacterController characterController;
     [SerializeField] private GameObject body;
     [SerializeField] private GameObject groundCheck;
     [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float panSpeed = 0.5f;
     [SerializeField] private float gravity = -10f;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private LayerMask groundMask;
 
-    private float panX = 0f;
+    private CharacterController characterController;
+    private PlayerCamera cam;
+    
     private Vector3 velocity;
     private bool isGrounded;
 
-    // Start is called before the first frame update
     void Awake()
     {
-        cam = GetComponentInChildren<Camera>();
         characterController = GetComponent<CharacterController>();
+        cam = GetComponent<PlayerCamera>();
     }
 
     void Start()
     {
         Assert.IsNotNull(body);
-        panX = Input.mousePosition.x;
+        Assert.IsNotNull(groundCheck);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        UpdateCamera();
         UpdateGravity();
         UpdateMovement();
-    }
-
-    private void UpdateCamera()
-    {
-        float pan = Input.mousePosition.x - panX;
-        panX = Input.mousePosition.x;
-        pan *= panSpeed;
-        cam.transform.RotateAround(transform.position, Vector3.up, pan);
     }
 
     private void UpdateGravity()
@@ -66,9 +53,9 @@ public class Player : MonoBehaviour
             MovePlayer(horizontal, vertical);
     }
 
-    void MovePlayer(float x, float z)
+    private void MovePlayer(float x, float z)
     {
-        Vector3 cameraDirection = cam.transform.forward;
+        Vector3 cameraDirection = cam.GetForwardVector();
         float cameraAngle = Mathf.Atan2(cameraDirection.z, cameraDirection.x);
         float inputAngle = Mathf.Atan2(z, x);
         float moveAngle = inputAngle - Mathf.PI / 2 + cameraAngle;
