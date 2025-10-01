95e81d98752cbd98b880adc4b68a7e66b711f2e1
RyanDeBarros <ryan2004db@gmail.com>
2025-10-01 17:18:48 -0400

    Added cooldown time to spell

--- Assets/Scripts/Spells/Melee/MeleeSpellCaster.cs
+++ Assets/Scripts/Spells/Melee/MeleeSpellCaster.cs
@@ -6,15 +6,18 @@ using UnityEngine.Assertions;
 public class MeleeSpellCaster : MonoBehaviour, ISpellCaster
 {
     [SerializeField] private GameObject spellPrefab;
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float cooldown = 0.5f;
+
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private int damage = 35;
-    [SerializeField] private float bounceBackStrength = 30f;
+    [SerializeField] private float bounceBackStrength = 200f;
 
     [SerializeField] private float lifetime = 0.3f;
     [SerializeField] private float shockwaveGrowSpeed = 10f;
     [SerializeField] private float blastGrowSpeed = 6f;
 
     private PlayerAnimatorController animator;
+    private float cooldownLeft = 0f;
 
     void Awake()
     {
@@ -23,8 +26,16 @@ public class MeleeSpellCaster : MonoBehaviour, ISpellCaster
         Assert.IsNotNull(spellPrefab);
     }
 
+    void Update()
+    {
+        if (cooldownLeft > 0f) cooldownLeft -= Time.deltaTime;
+    }
+
     public void CastSpell(Vector3 playerPosition, Vector3 staffPosition, Vector3 playerDirection, Vector3 cameraDirection)
     {
+        if (cooldownLeft > 0f) return;
+
+        cooldownLeft = cooldown;
         animator.ExecuteAttack1();
         GameObject instance = Instantiate(spellPrefab, playerPosition, Quaternion.LookRotation(playerDirection));
         MeleeSpell spell = instance.GetComponent<MeleeSpell>();
