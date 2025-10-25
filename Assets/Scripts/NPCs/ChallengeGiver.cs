335308ef8eb6e729b094d1167f97c8fe2377b6a4
RyanDeBarros <ryan2004db@gmail.com>
2025-10-25 00:49:50 -0400

    NPC has collision, always faces player, and has an [E] key hint floating above head

--- Assets/Scripts/NPCs/ChallengeGiver.cs
+++ Assets/Scripts/NPCs/ChallengeGiver.cs
@@ -1,42 +1,64 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Events;
 
 public class ChallengeGiver : MonoBehaviour
 {
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private float talkDistance = 10f;
+    [SerializeField] private TextMeshPro keyHint;
+
     public UnityEvent onConversationEnd;
 
     private ChallengeGiverAnimator animator;
 
+    private Coroutine spawnRoutine = null;
     private Coroutine despawnRoutine = null;
+    private Transform player;
 
     private void Awake()
     {
+        Assert.IsNotNull(keyHint);
+
         animator = GetComponentInChildren<ChallengeGiverAnimator>();
         Assert.IsNotNull(animator);
+
+        player = GameObject.FindGameObjectWithTag(playerTag).transform;
+        Assert.IsNotNull(player);
     }
 
     private void Start()
     {
         gameObject.SetActive(false);
+        keyHint.enabled = false;
     }
 
     private void Update()
     {
-        // TODO always face player
+        transform.LookAt(player.position);
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            // TODO start conversation if near player
-            DespawnNPC();
-        }
+        if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(transform.position, player.position) < talkDistance)
+            Talk();
+    }
+
+    private void Talk()
+    {
+        // TODO start conversation and call DespawnNPC() when done
+        DespawnNPC();
     }
 
     public void SpawnNPC()
     {
         gameObject.SetActive(true);
-        animator.AnimateSpawn();
+        spawnRoutine ??= StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        yield return animator.AnimateSpawn();
+        keyHint.enabled = true;
     }
 
     private void DespawnNPC()
@@ -46,6 +68,7 @@ public class ChallengeGiver : MonoBehaviour
 
     private IEnumerator DespawnRoutine()
     {
+        keyHint.enabled = false;
         yield return animator.AnimateDespawn();
         gameObject.SetActive(false);
         despawnRoutine = null;
