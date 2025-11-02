14eb36fcd51ccd665721936900bcec5506b279f6
RyanDeBarros <ryan2004db@gmail.com>
2025-11-02 17:54:16 -0500

    Implemented SPACE to dash

--- Assets/Scripts/Player/PlayerDash.cs
+++ Assets/Scripts/Player/PlayerDash.cs
@@ -1,18 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerDash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private PlayerCamera cam;
+    [SerializeField] private float inputDeadzone = 0.15f; // ignore tiny axis values
+
+    public float dashDuration = 0.15f;
+    public float dashSpeed = 30f;
+    // TODO UI for dash cooldown
+    public float cooldown = 1f;
+
+    private float timeElapsed = 0f;
+    private bool dashing = false;
+    private Vector3 dashDir = Vector3.zero;
+
+    private void Awake()
     {
-        
+        if (characterController == null)
+            characterController = GetComponent<CharacterController>();
+        Assert.IsNotNull(characterController);
+
+        if (cam == null)
+            cam = GetComponent<PlayerCamera>();
+        Assert.IsNotNull(cam);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (dashing)
+        {
+            timeElapsed -= Time.deltaTime;
+            if (timeElapsed > 0f)
+                characterController.Move(dashSpeed * Time.deltaTime * dashDir);
+            else
+            {
+                dashing = false;
+                timeElapsed = cooldown;
+            }
+        }
+        else
+        {
+            if (timeElapsed > 0f)
+                timeElapsed -= Time.deltaTime;
         
+            if (Input.GetKeyDown(KeyCode.Space))
+                TryDashing();
+        }
+    }
+
+    public void TryDashing()
+    {
+        if (dashing || timeElapsed > 0f)
+            return;
+
+        float horizontal = 0f;
+        float vertical = 0f;
+
+        if (Input.GetKey(KeyCode.A)) horizontal -= 1f;
+        if (Input.GetKey(KeyCode.D)) horizontal += 1f;
+
+        if (Input.GetKey(KeyCode.W)) vertical += 1f;
+        if (Input.GetKey(KeyCode.S)) vertical -= 1f;
+
+        if (Mathf.Abs(horizontal) > inputDeadzone || Mathf.Abs(vertical) > inputDeadzone)
+        {
+            dashing = true;
+            timeElapsed = dashDuration;
+
+            Vector3 forward = cam.GetForwardVector();
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = Vector3.Cross(Vector3.up, forward);
+            dashDir = (forward * vertical + right * horizontal).normalized;
+        }
     }
 }
