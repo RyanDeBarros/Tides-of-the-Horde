12a76f90d3cc322c2721c34bc3e515f1923a76b6
RyanDeBarros <ryan2004db@gmail.com>
2025-11-04 00:03:52 -0500

    Difficulty implementer

--- Assets/Scripts/Enemies/FlyingDemon/FlyingDemonDifficultyImplementer.cs
+++ Assets/Scripts/Enemies/FlyingDemon/FlyingDemonDifficultyImplementer.cs
@@ -5,37 +5,55 @@ using UnityEngine.Assertions;
 
 public class FlyingDemonDifficultyImplementer : MonoBehaviour, IDifficultyImplementer
 {
-    // TODO
-
     [Serializable]
     public class DifficultyStats
     {
-        //// DemonKingMovementAI
-        //public float moveSpeed = 10f;
-        //public float chaseRange = 30f;
+        // FlyingDemonMovement
+        public float moveSpeed = 5f;
+        public float chargeSpeed = 15f;
+        public float patrolSpeedMultiplier = 0.5f;
 
-        //public float sinkSpeed = 6f; // Speed at which boss sinks into ground
-        //public float sinkDepth = 5f; // How far underground to go
-        //public float teleportDuration = 3f; // Total time underground
-        //public float behindPlayerDistance = 4f; // Distance behind player to spawn
+        public float chaseRange = 50f;
+        public float runToRange = 15f;
+        public float stoppingDistance = 3.5f;
 
-        //// SwordHitbox
-        //public int damage = 5;
-        //public float bounceBackStrength = 75f;
+        public float sideStepSpeed = 10f;
+        public float sideStepDuration = 0.8f;
+        public float minSideStepCooldown = 1.5f;
+        public float maxSideStepCooldown = 3.0f;
 
-        //// TargetDetector
-        //public float attackRange = 7f;
-        //public float attackInterval = 1f;
+        // FlyingDemonAttackAI
+        public int punchDamage = 5;
+        public float punchBounceBackStrength = 120f;
+        public float punchProbabilityWeight = 1.0f;
+        public float punchAttackRange = 5f;
 
-        //// Health
-        //public int maxHealth = 1000;
+        public int biteDamage = 10;
+        public float biteBounceBackStrength = 100f;
+        public float biteProbabilityWeight = 0.75f;
+        public float biteAttackRange = 5f;
 
-        //// BounceBack
-        //public float bounceBackResistance = 2f;
-        //public float bounceBackDuration = 0.07f;
+        public int chargeDamage = 8;
+        public float chargeBounceBackStrength = 100f;
+        public float chargeProbabilityWeight = 0.3f;
+        public float chargeAttackRange = 8f;
+        public float chargeBackDistance = 3.5f;
+        public float chargeBackSpeed = 8f;
+        public float chargeForwardSpeed = 20f;
+        public float dizzyDuration = 2f;
 
-        //// RewardOnDeath
-        //public int reward = 10;
+        public float minAttackCooldown = 1.0f;
+        public float maxAttackCooldown = 2.0f;
+
+        // Health
+        public int maxHealth = 1000;
+
+        // BounceBack
+        public float bounceBackResistance = 6f;
+        public float bounceBackDuration = 0.1f;
+
+        // RewardOnDeath
+        public int reward = 50;
     }
 
     [Serializable]
@@ -48,12 +66,11 @@ public class FlyingDemonDifficultyImplementer : MonoBehaviour, IDifficultyImplem
 
     private static DifficultyStatsList difficultyStatsList = null;
 
-    //private DemonKingMovementAI movement;
-    //private SwordHitbox melee;
-    //private TargetDetector detector;
-    //private Health health;
-    //private BounceBack bounceBack;
-    //private RewardOnDeath reward;
+    private FlyingDemonMovement movement;
+    private FlyingDemonAttackAI attacker;
+    private Health health;
+    private BounceBack bounceBack;
+    private RewardOnDeath reward;
 
     private int difficultyLevel;
 
@@ -61,18 +78,16 @@ public class FlyingDemonDifficultyImplementer : MonoBehaviour, IDifficultyImplem
     {
         difficultyStatsList ??= JsonUtility.FromJson<DifficultyStatsList>(statsFile.text);
 
-        //movement = GetComponent<DemonKingMovementAI>();
-        //Assert.IsNotNull(movement);
-        //melee = GetComponentInChildren<SwordHitbox>();
-        //Assert.IsNotNull(melee);
-        //detector = GetComponent<TargetDetector>();
-        //Assert.IsNotNull(detector);
-        //health = GetComponent<Health>();
-        //Assert.IsNotNull(health);
-        //bounceBack = GetComponent<BounceBack>();
-        //Assert.IsNotNull(bounceBack);
-        //reward = GetComponent<RewardOnDeath>();
-        //Assert.IsNotNull(reward);
+        movement = GetComponent<FlyingDemonMovement>();
+        Assert.IsNotNull(movement);
+        attacker = GetComponent<FlyingDemonAttackAI>();
+        Assert.IsNotNull(attacker);
+        health = GetComponent<Health>();
+        Assert.IsNotNull(health);
+        bounceBack = GetComponent<BounceBack>();
+        Assert.IsNotNull(bounceBack);
+        reward = GetComponent<RewardOnDeath>();
+        Assert.IsNotNull(reward);
     }
 
     public void SetDifficultyLevel(int level)
@@ -80,25 +95,47 @@ public class FlyingDemonDifficultyImplementer : MonoBehaviour, IDifficultyImplem
         difficultyLevel = Math.Clamp(level, 1, difficultyStatsList.stats.Count);
         DifficultyStats stats = difficultyStatsList.stats[difficultyLevel - 1];
 
-        //movement.moveSpeed = stats.moveSpeed;
-        //movement.chaseRange = stats.chaseRange;
-        //movement.sinkSpeed = stats.sinkSpeed;
-        //movement.sinkDepth = stats.sinkDepth;
-        //movement.teleportDuration = stats.teleportDuration;
-        //movement.behindPlayerDistance = stats.behindPlayerDistance;
+        movement.moveSpeed = stats.moveSpeed;
+        movement.chargeSpeed = stats.chargeSpeed;
+        movement.patrolSpeedMultiplier = stats.patrolSpeedMultiplier;
 
-        //melee.damage = stats.damage;
-        //melee.bounceBackStrength = stats.bounceBackStrength;
+        movement.chaseRange = stats.chaseRange;
+        movement.runToRange = stats.runToRange;
+        movement.stoppingDistance = stats.stoppingDistance;
 
-        //detector.attackRange = stats.attackRange;
-        //detector.attackInterval = stats.attackInterval;
+        movement.sideStepSpeed = stats.sideStepSpeed;
+        movement.sideStepDuration = stats.sideStepDuration;
+        movement.minSideStepCooldown = stats.minSideStepCooldown;
+        movement.maxSideStepCooldown = stats.maxSideStepCooldown;
 
-        //health.maxHealth = stats.maxHealth;
+        attacker.punchDamage = stats.punchDamage;
+        attacker.punchBounceBackStrength = stats.punchBounceBackStrength;
+        attacker.punchProbabilityWeight = stats.punchProbabilityWeight;
+        attacker.punchAttackRange = stats.punchAttackRange;
 
-        //bounceBack.resistance = stats.bounceBackResistance;
-        //bounceBack.duration = stats.bounceBackDuration;
+        attacker.biteDamage = stats.punchDamage;
+        attacker.biteBounceBackStrength = stats.punchBounceBackStrength;
+        attacker.biteProbabilityWeight = stats.punchProbabilityWeight;
+        attacker.biteAttackRange = stats.punchAttackRange;
 
-        //reward.reward = stats.reward;
+        attacker.chargeDamage = stats.chargeDamage;
+        attacker.chargeBounceBackStrength = stats.chargeBounceBackStrength;
+        attacker.chargeProbabilityWeight = stats.chargeProbabilityWeight;
+        attacker.chargeAttackRange = stats.chargeAttackRange;
+        attacker.chargeBackDistance = stats.chargeBackDistance;
+        attacker.chargeBackSpeed = stats.chargeBackSpeed;
+        attacker.chargeForwardSpeed = stats.chargeForwardSpeed;
+        attacker.dizzyDuration = stats.dizzyDuration;
+
+        attacker.minCooldown = stats.minAttackCooldown;
+        attacker.maxCooldown = stats.maxAttackCooldown;
+
+        health.maxHealth = stats.maxHealth;
+
+        bounceBack.resistance = stats.bounceBackResistance;
+        bounceBack.duration = stats.bounceBackDuration;
+
+        reward.reward = stats.reward;
     }
 
     public int GetDifficultyLevel()
