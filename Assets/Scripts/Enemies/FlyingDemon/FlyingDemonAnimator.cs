44c60ddd420ee8e0c6cca6aaef5c15679781ef25
RyanDeBarros <ryan2004db@gmail.com>
2025-11-03 21:20:53 -0500

    Implemented animator

--- Assets/Scripts/Enemies/FlyingDemon/FlyingDemonAnimator.cs
+++ Assets/Scripts/Enemies/FlyingDemon/FlyingDemonAnimator.cs
@@ -10,5 +10,94 @@ public class FlyingDemonAnimator : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         Assert.IsNotNull(animator);
+
+        Health health = GetComponentInParent<Health>();
+        Assert.IsNotNull(health);
+        health.onHealthThresholdReached.AddListener(GetHitAnimation);
+    }
+
+    private void GetHitAnimation()
+    {
+        animator.SetTrigger("GetHit");
+    }
+
+    public void SetIdle()
+    {
+        CancelDizzy();
+        CancelSideStep();
+        animator.SetInteger("MoveVec", 0);
+    }
+
+    public void SetMovingForward()
+    {
+        CancelDizzy();
+        CancelSideStep();
+        animator.SetInteger("MoveVec", 1);
+    }
+
+    public void SetMovingBackward()
+    {
+        CancelDizzy();
+        CancelSideStep();
+        animator.SetInteger("MoveVec", -1);
+    }
+
+    public void SetRunning()
+    {
+        CancelDizzy();
+        CancelSideStep();
+        animator.SetInteger("MoveVec", 2);
+    }
+
+    public void SetMovingLeft()
+    {
+        CancelDizzy();
+        CancelMove();
+        animator.SetInteger("SideStep", -1);
+    }
+
+    public void SetMovingRight()
+    {
+        CancelDizzy();
+        CancelMove();
+        animator.SetInteger("SideStep", 1);
+    }
+
+    public void SetDizzy()
+    {
+        CancelMove();
+        CancelSideStep();
+        animator.SetBool("Dizzy", true);
+    }
+
+    private void CancelMove()
+    {
+        animator.SetInteger("MoveVec", 0);
+    }
+
+    private void CancelSideStep()
+    {
+        animator.SetInteger("SideStep", 0);
+    }
+
+    private void CancelDizzy()
+    {
+        animator.SetBool("Dizzy", false);
+    }
+
+    public void PunchAttack()
+    {
+        animator.SetTrigger("Attack1");
+    }
+
+    public void BigBiteAttack()
+    {
+        animator.SetTrigger("Attack2");
+    }
+
+    // TODO Do dash attack into small bite. If bubble spell is active, stun boss -> set dizzy.
+    public void SmallBiteAttack()
+    {
+        animator.SetTrigger("Attack3");
     }
 }
