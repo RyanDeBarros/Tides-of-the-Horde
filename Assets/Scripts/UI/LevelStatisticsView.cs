ea9b35035608ba0bf2a37330e3f5e49fc4ee7e5c
RyanDeBarros <ryan2004db@gmail.com>
2025-11-08 23:46:56 -0500

    Fully implemented level statistics scene

--- Assets/Scripts/UI/LevelStatisticsView.cs
+++ Assets/Scripts/UI/LevelStatisticsView.cs
@@ -4,15 +4,34 @@ using UnityEngine.Assertions;
 
 public class LevelStatisticsView : MonoBehaviour
 {
+    [Header("Main Stats")]
     [SerializeField] private TextMeshProUGUI finalHealthText;
     [SerializeField] private TextMeshProUGUI totalCurrencyText;
     [SerializeField] private TextMeshProUGUI totalTimeText;
+
+    [Header("Enemies Defeated")]
+    [SerializeField] private TextMeshProUGUI skeletonsText;
+    [SerializeField] private TextMeshProUGUI orcsText;
+    [SerializeField] private TextMeshProUGUI bishopsText;
+    [SerializeField] private TextMeshProUGUI dragonsText;
+
+    [Header("Bosses Defeated")]
+    [SerializeField] private TextMeshProUGUI flyingDemonsText;
+    [SerializeField] private TextMeshProUGUI demonKingsText;
 
     private void Awake()
     {
         Assert.IsNotNull(finalHealthText);
         Assert.IsNotNull(totalCurrencyText);
         Assert.IsNotNull(totalTimeText);
+
+        Assert.IsNotNull(skeletonsText);
+        Assert.IsNotNull(orcsText);
+        Assert.IsNotNull(bishopsText);
+        Assert.IsNotNull(dragonsText);
+
+        Assert.IsNotNull(flyingDemonsText);
+        Assert.IsNotNull(demonKingsText);
     }
 
     private void Start()
@@ -22,15 +41,22 @@ public class LevelStatisticsView : MonoBehaviour
         int totalTime = (int)LevelStatistics.GetTotalTime();
         totalTimeText.SetText($"{(totalTime / 60)}:{totalTime % 60 :D2}");
 
-        // TODO display stastics in UI
-        Debug.Log($"# skeletons defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.Skeleton)}");
-        Debug.Log($"# orcs defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.Orc)}");
-        Debug.Log($"# bishops defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.Bishop)}");
-        Debug.Log($"# dragons defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.Dragon)}");
-        Debug.Log($"# flying demons defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.FlyingDemon)}");
-        Debug.Log($"# demon kings defeated = {LevelStatistics.GetEnemiesDefeated(EnemyType.DemonKing)}");
+        void SetNumberOfEnemies(EnemyType enemyType, TextMeshProUGUI enemyText)
+        {
+            int enemies = LevelStatistics.GetEnemiesDefeated(enemyType);
+            enemyText.SetText($"{enemies}");
+            enemyText.transform.parent.gameObject.SetActive(enemies > 0);
+        }
+
+        SetNumberOfEnemies(EnemyType.Skeleton, skeletonsText);
+        SetNumberOfEnemies(EnemyType.Orc, orcsText);
+        SetNumberOfEnemies(EnemyType.Bishop, bishopsText);
+        SetNumberOfEnemies(EnemyType.Dragon, dragonsText);
+        SetNumberOfEnemies(EnemyType.FlyingDemon, flyingDemonsText);
+        SetNumberOfEnemies(EnemyType.DemonKing, demonKingsText);
     }
 
+    // TODO continue button
     public void OnContinue()
     {
         SceneSwitcher.OpenLevelSelect();
