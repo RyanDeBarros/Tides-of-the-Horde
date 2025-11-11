15b44e6765aec354e5bab130b3c4bebe62dafc24
RyanDeBarros <ryan2004db@gmail.com>
2025-11-10 21:36:48 -0500

    Potion prefab that despawns when collected by player

---
+++ Assets/Scripts/Enemies/DemonKing/Potion.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Potion : MonoBehaviour
+{
+    [SerializeField] private float collectDespawnDuration = 0.5f;
+
+    private Coroutine collectRoutine = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Assert.IsTrue(other.CompareTag("Player"));
+        collectRoutine ??= StartCoroutine(Collect());
+    }
+
+    private IEnumerator Collect()
+    {
+        transform.localScale = Vector3.one;
+
+        for (float t = 0; t < collectDespawnDuration; t += Time.deltaTime)
+        {
+            float a = 1f - t / collectDespawnDuration;
+            transform.localScale = new Vector3(a, a, a);
+            yield return null;
+        }
+
+        transform.localScale = Vector3.zero;
+        Destroy(gameObject);
+        // TODO increment player's collectible count
+    }
+}
