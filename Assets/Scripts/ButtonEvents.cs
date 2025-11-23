98a77399b1b287afcdff174fffc023259886839f
RyanDeBarros <ryan2004db@gmail.com>
2025-11-23 00:34:51 -0500

    Full demon king SFX

--- Assets/Scripts/ButtonEvents.cs
+++ Assets/Scripts/ButtonEvents.cs
@@ -13,8 +13,6 @@ public class ButtonSFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandl
         {
             // try find a global UI AudioSource in scene
             audioSource = FindFirstObjectByType<AudioSource>();
-            if (audioSource != null)
-                Debug.Log($"[ButtonSFX] Auto-assigned AudioSource from scene to '{gameObject.name}'");
         }
 
         if (audioSource == null)
--- Assets/Scripts/Enemies/DemonKing/DemonKingAnimator.cs
+++ Assets/Scripts/Enemies/DemonKing/DemonKingAnimator.cs
@@ -6,6 +6,9 @@ public class DemonKingAnimator : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private DemonKingMovementAI movement;
     [SerializeField] private DemonKingAttackAI attackAI;
+    [SerializeField] private AudioClip attack1Clip;
+    [SerializeField] private AudioClip attack2Clip;
+    [SerializeField] private AudioSource audioSource;
 
     private bool movementLocked = false;
 
@@ -22,30 +25,42 @@ public class DemonKingAnimator : MonoBehaviour
         if (attackAI == null)
             attackAI = GetComponentInParent<DemonKingAttackAI>();
         Assert.IsNotNull(attackAI);
+
+        if (audioSource == null)
+            audioSource = GetComponentInParent<AudioSource>();
+        Assert.IsNotNull(audioSource);
     }
 
     public void TriggerAttack1()
     {
         movementLocked = true;
         animator.SetTrigger("Attack1");
+        if (attack1Clip != null)
+            audioSource.PlayOneShot(attack1Clip);
     }
 
     public void TriggerAttack2()
     {
         movementLocked = true;
         animator.SetTrigger("Attack2");
+        if (attack2Clip != null)
+            audioSource.PlayOneShot(attack2Clip);
     }
 
     public void TriggerComboAttack()
     {
         movementLocked = true;
         animator.SetTrigger("ComboAttack");
+        if (attack1Clip != null)
+            audioSource.PlayOneShot(attack1Clip);
     }
 
     // Called by animator
     public void OnComboAttackReadjust()
     {
         movement.FacePlayer();
+        if (attack2Clip != null)
+            audioSource.PlayOneShot(attack2Clip);
     }
 
     // Called by animator
@@ -74,6 +89,7 @@ public class DemonKingAnimator : MonoBehaviour
     {
         movement.StartTeleportSequence();
         movementLocked = false;
+        // TODO get hit sfx for bosses
     }
 
     public void TriggerTelegraph()
